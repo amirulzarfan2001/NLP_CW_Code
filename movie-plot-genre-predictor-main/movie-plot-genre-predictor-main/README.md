28d12cd9735333b7b1c0641a4f4f2c2c9bcca54f
amirulzarfan2001 <43676599+amirulzarfan2001@users.noreply.github.com>
2025-04-04 23:43:49 +0100

    Cleaned everything about anime dataset

---
+++ movie-plot-genre-predictor-main/movie-plot-genre-predictor-main/README.md
@@ -0,0 +1,4 @@
+# movie-plot-genre-predictor
+Training a Transformer model to predict movie genres
+
+https://www.youtube.com/watch?v=Gme2lw7xRfc&ab_channel=langfab
---
+++ my_import.py
@@ -0,0 +1,25 @@
+import kaggle
+import kagglehub
+import numpy as np
+import pandas as pd
+from IPython.display import display
+from kagglehub import KaggleDatasetAdapter
+from collections import Counter
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+__all__ = [
+    'kaggle', 
+    'kagglehub', 
+    'np', 
+    'pd', 
+    'display', 
+    'KaggleDatasetAdapter', 
+    'Counter', 
+    'plt', 
+    'MultiLabelBinarizer',
+    'CountVectorizer'
+]
